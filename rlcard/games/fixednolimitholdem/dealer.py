bbcafb0b836416ca3f50d7fe0cd7704a0db6b224
qialex <qialex.i@gmail.com>
2025-04-20 01:26:15 +0200

    add manual dealer for controlling the cards dealt

--- rlcard/games/fixednolimitholdem/dealer.py
+++ rlcard/games/fixednolimitholdem/dealer.py
@@ -2,4 +2,123 @@ from rlcard.games.limitholdem import Dealer
 
 
 class NolimitholdemDealer(Dealer):
-    pass
+    def __init__(self, np_random):
+        super().__init__(np_random)
+        # Initialize new properties with default values
+        self.preset_player0_hand = []
+        self.preset_flop = []
+        self.preset_turn = None
+        self.preset_river = None
+        self.current_stage = None
+        self.manual_mode = False  # Default to automatic dealing
+    
+    def enable_manual_mode(self):
+        """Enable manual card selection mode"""
+        self.manual_mode = True
+    
+    def set_player0_hand(self, cards):
+        """Set specific cards for Player 0's hand
+        
+        Args:
+            cards (list): List of card objects to be used as Player 0's hand
+        """
+        if not self.manual_mode:
+            return  # Do nothing if not in manual mode
+            
+        self.preset_player0_hand = cards
+        # Remove these cards from the deck
+        for card in cards:
+            if card in self.deck:
+                self.deck.remove(card)
+    
+    def set_flop(self, cards):
+        """Set specific flop cards
+        
+        Args:
+            cards (list): List of 3 card objects to be used as flop
+        """
+        if not self.manual_mode:
+            return  # Do nothing if not in manual mode
+            
+        if len(cards) != 3:
+            raise ValueError("Flop must consist of exactly 3 cards")
+        self.preset_flop = cards
+        # Remove these cards from the deck
+        for card in cards:
+            if card in self.deck:
+                self.deck.remove(card)
+    
+    def set_turn(self, card):
+        """Set specific turn card
+        
+        Args:
+            card (object): Card object to be used as turn
+        """
+        if not self.manual_mode:
+            return  # Do nothing if not in manual mode
+            
+        self.preset_turn = card
+        # Remove this card from the deck
+        if card in self.deck:
+            self.deck.remove(card)
+    
+    def set_river(self, card):
+        """Set specific river card
+        
+        Args:
+            card (object): Card object to be used as river
+        """
+        if not self.manual_mode:
+            return  # Do nothing if not in manual mode
+            
+        self.preset_river = card
+        # Remove this card from the deck
+        if card in self.deck:
+            self.deck.remove(card)
+    
+    def has_preset_cards(self, stage):
+        """Check if dealer has preset cards for the given stage
+        
+        Args:
+            stage (str): The stage to check ('flop', 'turn', or 'river')
+            
+        Returns:
+            (bool): True if dealer has preset cards for the stage
+        """
+        if not self.manual_mode:
+            return True  # In automatic mode, we always have cards
+            
+        if stage == 'flop':
+            return len(self.preset_flop) == 3
+        elif stage == 'turn':
+            return self.preset_turn is not None
+        elif stage == 'river':
+            return self.preset_river is not None
+        return False
+    
+    def deal_card(self):
+        """Deal a card from the deck
+        
+        Returns:
+            (object): A card object
+        """
+        # Only use preset cards if in manual mode
+        if self.manual_mode:
+            # For player 0's hand
+            if len(self.preset_player0_hand) > 0:
+                return self.preset_player0_hand.pop(0)
+            
+            # For community cards based on current stage
+            if self.current_stage == 'flop' and len(self.preset_flop) > 0:
+                return self.preset_flop.pop(0)
+            elif self.current_stage == 'turn' and self.preset_turn is not None:
+                card = self.preset_turn
+                self.preset_turn = None
+                return card
+            elif self.current_stage == 'river' and self.preset_river is not None:
+                card = self.preset_river
+                self.preset_river = None
+                return card
+        
+        # Default behavior - deal from deck
+        return super().deal_card()
--- rlcard/games/fixednolimitholdem/game.py
+++ rlcard/games/fixednolimitholdem/game.py
@@ -1,5 +1,4 @@
 from enum import Enum
-
 import numpy as np
 from copy import deepcopy
 from rlcard.games.limitholdem import Game
@@ -18,6 +17,9 @@ class Stage(Enum):
     RIVER = 3
     END_HIDDEN = 4
     SHOWDOWN = 5
+    WAITING_FOR_FLOP = 6  # Waiting for flop cards
+    WAITING_FOR_TURN = 7  # Waiting for turn card
+    WAITING_FOR_RIVER = 8  # Waiting for river card
 
 
 class NolimitholdemGame(Game):
@@ -36,6 +38,10 @@ class NolimitholdemGame(Game):
 
         # If None, the dealer will be randomly chosen
         self.dealer_id = None
+        
+        # For manually setting cards
+        self.player0_hand = []
+        self.manual_dealer = False  # Default to automatic dealing
 
     def configure(self, game_config):
         """
@@ -46,6 +52,13 @@ class NolimitholdemGame(Game):
         # must have num_players length
         self.init_chips = [game_config['chips_for_each']] * game_config["game_num_players"]
         self.dealer_id = game_config['dealer_id']
+        
+        # Check for manual dealer flag
+        self.manual_dealer = game_config.get('manual_dealer', False)
+        
+        # Set player0 hand if provided and manual dealer is enabled
+        if self.manual_dealer and 'player0_hand' in game_config:
+            self.player0_hand = game_config['player0_hand']
 
     def init_game(self):
         """
@@ -64,9 +77,17 @@ class NolimitholdemGame(Game):
         else:
             self.dealer_id = (self.dealer_id + 1) % self.num_players
             
-
         # Initialize a dealer that can deal cards
         self.dealer = Dealer(self.np_random)
+        
+        # Enable manual mode if configured
+        if self.manual_dealer:
+            if hasattr(self.dealer, 'enable_manual_mode'):
+                self.dealer.enable_manual_mode()
+            
+            # Set preset cards for Player 0's hand if provided
+            if self.player0_hand and hasattr(self.dealer, 'set_player0_hand'):
+                self.dealer.set_player0_hand(self.player0_hand)
 
         # Initialize players to play the game
         self.players = [Player(i, self.init_chips[i], self.np_random) for i in range(self.num_players)]
@@ -74,7 +95,7 @@ class NolimitholdemGame(Game):
         # Initialize a judger class which will decide who wins in the end
         self.judger = Judger(self.np_random)
 
-        # Deal cards to each  player to prepare for the first round
+        # Deal cards to each player to prepare for the first round
         for i in range(2 * self.num_players):
             self.players[i % self.num_players].hand.append(self.dealer.deal_card())
 
@@ -112,6 +133,77 @@ class NolimitholdemGame(Game):
         state = self.get_state(self.game_pointer)
 
         return state, self.game_pointer
+    
+    def set_flop(self, cards):
+        """Set specific flop cards
+        
+        Args:
+            cards (list): List of 3 card objects to be used as flop
+        """
+        if not self.manual_dealer:
+            return  # Do nothing if not in manual mode
+            
+        if hasattr(self.dealer, 'set_flop'):
+            self.dealer.set_flop(cards)
+            
+            # If we were waiting for flop cards, resume the game
+            if self.stage == Stage.WAITING_FOR_FLOP:
+                self._deal_flop()
+                self.stage = Stage.FLOP
+    
+    def set_turn(self, card):
+        """Set specific turn card
+        
+        Args:
+            card (object): Card object to be used as turn
+        """
+        if not self.manual_dealer:
+            return  # Do nothing if not in manual mode
+            
+        if hasattr(self.dealer, 'set_turn'):
+            self.dealer.set_turn(card)
+            
+            # If we were waiting for turn card, resume the game
+            if self.stage == Stage.WAITING_FOR_TURN:
+                self._deal_turn()
+                self.stage = Stage.TURN
+    
+    def set_river(self, card):
+        """Set specific river card
+        
+        Args:
+            card (object): Card object to be used as river
+        """
+        if not self.manual_dealer:
+            return  # Do nothing if not in manual mode
+            
+        if hasattr(self.dealer, 'set_river'):
+            self.dealer.set_river(card)
+            
+            # If we were waiting for river card, resume the game
+            if self.stage == Stage.WAITING_FOR_RIVER:
+                self._deal_river()
+                self.stage = Stage.RIVER
+    
+    def _deal_flop(self):
+        """Deal the flop cards"""
+        if hasattr(self.dealer, 'current_stage'):
+            self.dealer.current_stage = 'flop'
+        self.public_cards.append(self.dealer.deal_card())
+        self.public_cards.append(self.dealer.deal_card())
+        self.public_cards.append(self.dealer.deal_card())
+    
+    def _deal_turn(self):
+        """Deal the turn card"""
+        if hasattr(self.dealer, 'current_stage'):
+            self.dealer.current_stage = 'turn'
+        self.public_cards.append(self.dealer.deal_card())
+    
+    def _deal_river(self):
+        """Deal the river card"""
+        if hasattr(self.dealer, 'current_stage'):
+            self.dealer.current_stage = 'river'
+        self.public_cards.append(self.dealer.deal_card())
 
     def get_legal_actions(self):
         """
@@ -120,6 +212,10 @@ class NolimitholdemGame(Game):
         Returns:
             (list): A list of legal actions
         """
+        # If we're waiting for manual cards, no betting actions are allowed
+        if self.stage in (Stage.WAITING_FOR_FLOP, Stage.WAITING_FOR_TURN, Stage.WAITING_FOR_RIVER):
+            return []  # No betting actions allowed while waiting for cards
+            
         return self.round.get_nolimit_legal_actions(players=self.players)
 
     def step(self, action):
@@ -135,6 +231,9 @@ class NolimitholdemGame(Game):
                 (dict): next player's state
                 (int): next player id
         """
+        # If we're waiting for manual cards, don't allow any actions
+        if self.stage in (Stage.WAITING_FOR_FLOP, Stage.WAITING_FOR_TURN, Stage.WAITING_FOR_RIVER):
+            raise Exception('Cannot take actions while waiting for manual cards')
 
         if action not in self.get_legal_actions():
             print(action, self.get_legal_actions())
@@ -169,27 +268,53 @@ class NolimitholdemGame(Game):
                 while players_in_bypass[self.game_pointer]:
                     self.game_pointer = (self.game_pointer + 1) % self.num_players
 
-            # For the first round, we deal 3 cards
-            if self.round_counter == 0:
-                self.stage = Stage.FLOP
-                self.public_cards.append(self.dealer.deal_card())
-                self.public_cards.append(self.dealer.deal_card())
-                self.public_cards.append(self.dealer.deal_card())
-                if len(self.players) == np.sum(players_in_bypass):
-                    self.round_counter += 1
-            # For the following rounds, we deal only 1 card
-            if self.round_counter == 1:
-                self.stage = Stage.TURN
-                self.public_cards.append(self.dealer.deal_card())
-                if len(self.players) == np.sum(players_in_bypass):
-                    self.round_counter += 1
-            if self.round_counter == 2:
-                self.stage = Stage.RIVER
-                self.public_cards.append(self.dealer.deal_card())
-                if len(self.players) == np.sum(players_in_bypass):
-                    self.round_counter += 1
-
+            # Handle the end of each betting round
+            if self.round_counter == 0:  # End of preflop
+                if self.manual_dealer:
+                    # Check if we have preset flop cards
+                    if hasattr(self.dealer, 'has_preset_cards') and self.dealer.has_preset_cards('flop'):
+                        self._deal_flop()
+                        self.stage = Stage.FLOP
+                    else:
+                        # Wait for flop cards to be set
+                        self.stage = Stage.WAITING_FOR_FLOP
+                else:
+                    # Automatic dealer mode
+                    self._deal_flop()
+                    self.stage = Stage.FLOP
+                
+            elif self.round_counter == 1:  # End of flop
+                if self.manual_dealer:
+                    # Check if we have preset turn card
+                    if hasattr(self.dealer, 'has_preset_cards') and self.dealer.has_preset_cards('turn'):
+                        self._deal_turn()
+                        self.stage = Stage.TURN
+                    else:
+                        # Wait for turn card to be set
+                        self.stage = Stage.WAITING_FOR_TURN
+                else:
+                    # Automatic dealer mode
+                    self._deal_turn()
+                    self.stage = Stage.TURN
+                
+            elif self.round_counter == 2:  # End of turn
+                if self.manual_dealer:
+                    # Check if we have preset river card
+                    if hasattr(self.dealer, 'has_preset_cards') and self.dealer.has_preset_cards('river'):
+                        self._deal_river()
+                        self.stage = Stage.RIVER
+                    else:
+                        # Wait for river card to be set
+                        self.stage = Stage.WAITING_FOR_RIVER
+                else:
+                    # Automatic dealer mode
+                    self._deal_river()
+                    self.stage = Stage.RIVER
+            
+            # Increment round counter
             self.round_counter += 1
+            
+            # Start a new bidding round
             self.round.start_new_round(self.game_pointer)
 
         state = self.get_state(self.game_pointer)
@@ -215,47 +340,12 @@ class NolimitholdemGame(Game):
         state['current_player'] = self.game_pointer
         state['pot'] = self.dealer.pot
         state['stage'] = self.stage
+        
+        # Add waiting_for_cards flag for manual dealer mode
+        if self.stage in (Stage.WAITING_FOR_FLOP, Stage.WAITING_FOR_TURN, Stage.WAITING_FOR_RIVER):
+            state['waiting_for_cards'] = True
+            state['waiting_stage'] = self.stage
+        else:
+            state['waiting_for_cards'] = False
+            
         return state
-
-    def step_back(self):
-        """
-        Return to the previous state of the game
-
-        Returns:
-            (bool): True if the game steps back successfully
-        """
-        if len(self.history) > 0:
-            self.round, self.game_pointer, self.round_counter, self.dealer, self.public_cards, self.players = self.history.pop()
-            self.stage = Stage(self.round_counter)
-            return True
-        return False
-
-    def get_num_players(self):
-        """
-        Return the number of players in no limit texas holdem
-
-        Returns:
-            (int): The number of players in the game
-        """
-        return self.num_players
-
-    def get_payoffs(self):
-        """
-        Return the payoffs of the game
-
-        Returns:
-            (list): Each entry corresponds to the payoff of one player
-        """
-        hands = [p.hand + self.public_cards if p.status in (PlayerStatus.ALIVE, PlayerStatus.ALLIN) else None for p in self.players]
-        chips_payoffs = self.judger.judge_game(self.players, hands)
-        return chips_payoffs
-
-    @staticmethod
-    def get_num_actions():
-        """
-        Return the number of applicable actions
-
-        Returns:
-            (int): The number of actions. There are 6 actions (call, raise_half_pot, raise_pot, all_in, check and fold)
-        """
-        return len(Action)
